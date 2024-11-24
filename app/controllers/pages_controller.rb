746853039c8b68b1c7fee9dedd9f998487f29f9d
Ali Aizad <aliaizad72@gmail.com>
2024-11-24 16:39:11 +0800

    Search analytics page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,19 @@ class PagesController < ApplicationController
   def home
     @url = Url.new
   end
+  def analytics
+    @short = params[:short].split("/")[-1]
+    @url = Url.find_by(short: @short)
+
+    if @url.nil?
+      flash[:alert] = "No such links found"
+      redirect_to search_path
+    else
+      redirect_to url_path(@url)
+    end
+  end
+
+  def search
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "/home", to: "pages#home"
+  get "/analytics", to: "pages#analytics"
+  get "/search", to: "pages#search"
 
   # Defines the root path route ("/")
   root "pages#home"
